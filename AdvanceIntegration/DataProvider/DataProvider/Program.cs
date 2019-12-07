c99300c4fe64fcac47ba286de33d84d4023e620c
Michael Pendon <michael.c.pendon@outlook.com>
2019-12-07 21:00:27 +0100

    Added the factories and processors and updated the connection string to Inventory database.

--- AdvanceIntegration/DataProvider/DataProvider/Program.cs
+++ AdvanceIntegration/DataProvider/DataProvider/Program.cs
@@ -1,10 +1,6 @@
-﻿using DataProvider.Models;
-using DataProvider.Repositories;
-using RepoDb;
-using RepoDb.Extensions;
+﻿using DataProvider.Factories;
+using DataProvider.Processor;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace DataProvider
 {
@@ -23,44 +19,12 @@ namespace DataProvider
 
         private static void Process(int count)
         {
-            var messages = GetMessages(count)
-                .AsList();
-
-            using (var repository = new MessageRepository())
-            {
-                var startTime = DateTime.UtcNow;
-                var deleteAllResult = repository.DeleteAll();
-
-                Console.WriteLine($"Truncated the table. " +
-                    $"Elapsed: { (DateTime.UtcNow - startTime).TotalSeconds } second(s).");
-
-                startTime = DateTime.UtcNow;
-                var bulkInsertResult = repository.BulkInsert(messages);
-                Console.WriteLine($"{bulkInsertResult} message(s) has been inserted. " +
-                    $"Elapsed: { (DateTime.UtcNow - startTime).TotalSeconds } second(s).");
-            }
-        }
-
-        private static IEnumerable<Message> GetMessages(int count)
-        {
-            var strings = GetStrings().AsList();
-            for (var i = 0; i < count; i++)
-            {
-                var index = new Random().Next(0, strings.Count - 1);
-                yield return new Message
-                {
-                    Body = strings[index]
-                };
-            }
-        }
-
-        private static IEnumerable<string> GetStrings()
-        {
-            var text = @"We believe that it's time to take real action to create a world that runs on green energy. Renewable energy holds the key to a cleaner future, and we need to act now to reduce the effects of climate change.";
-            return text.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                .Select(s => s?.Trim())
-                .Where(s => s?.Length > 0)
-                .AsEnumerable();
+            var messages = MessageFactory.GetMessages(count);
+            var processor = new MessageDataProcessor();
+            var startTime = DateTime.UtcNow;
+            var result = processor.Process(messages);
+            Console.WriteLine($"{result} message(s) has been inserted. " +
+                $"Elapsed: { (DateTime.UtcNow - startTime).TotalSeconds } second(s).");
         }
     }
 }
--- AdvanceIntegration/DataProvider/DataProvider/Repositories/MessageRepository.cs
+++ AdvanceIntegration/DataProvider/DataProvider/Repositories/MessageRepository.cs
@@ -7,7 +7,7 @@ namespace DataProvider.Repositories
     public class MessageRepository : BaseRepository<Message, SqlConnection>
     {
         public MessageRepository()
-            : base("Server=tcp:adventureworksapi.database.windows.net,1433;Initial Catalog=DataHub;Persist Security Info=False;User ID=user;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+            : base("Server=tcp:mikependonsqlserver.database.windows.net,1433;Initial Catalog=Inventory;Persist Security Info=False;User ID=user;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
         { }
     }
 }
